43fe08ab33b0d4c9e1b78386b0260beb40ec0a10
Yurii Yashchuk <106056914+IronFather@users.noreply.github.com>
2022-12-30 09:56:13 +0200

    resumed repository (my part-footer)

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './js/counter';
+// import './js/gallery';
+import './js/mobile-menu';
+import './js/modal';
+import './js/scroll';
+import './js/scrolltoup';
+import './js/slick.min';
+import './js/slider';
